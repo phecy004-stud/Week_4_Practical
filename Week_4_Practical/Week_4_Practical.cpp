5a3dbd16bb9dd73e197855338361cd5b7d7afde4
Phe <phecy004@mymail.unisa.edu.au>
2025-03-27 13:02:08 +1030

    Commit changes made in Week_4_Practical.cpp following step 3 in the Practical Worksheet (still working on it)

--- Week_4_Practical/Week_4_Practical.cpp
+++ Week_4_Practical/Week_4_Practical.cpp
@@ -2,11 +2,41 @@
 //
 
 #include "Week_4_Practical.h"
+#include <iostream>
+#include <list>
+#include <set>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+
+
+struct ListComparator {
+    bool operator()(const std::list<int>& lhs, const std::list<int>& rhs) const {
+        return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+};
+
+int main() {
+
+    std::cout << "Please enter a set of numbers (enter '0' to end):\n";
+
+    std::list<int> numbers;
+    int num;
+    while (std::cin >> num && num != 0) {
+        numbers.push_back(num);
+    }
+
+    std::set<std::list<int>> n;
+    n.insert(numbers);
+
+    std::cout << "Inserted list:\n";
+
+    for (const auto list : n) {
+        for (int val : list) {
+            std::cout << val << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
 }
